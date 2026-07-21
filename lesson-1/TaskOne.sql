f6778d3e13b5fe401c34766c1416ad83cc44757c
Oscar <oguzman@basecampcodingacademy.org>
2026-07-21 17:34:58 -0500

    Module 3

--- lesson-1/TaskOne.sql
+++ lesson-1/TaskOne.sql
@@ -1,14 +1,19 @@
 -- Edit this code below
 -- 1. Add a CREATE TABLE command.
+
 --    1.1 Give the table a name
 --    1.2 We want to store firstname, surname, month born and age.
 --    1.3 Set the right data types; varchar - used for text or text and numbers like a car number plate. int - used for numbers.
 
-
+CREATE TABLE people (firstname VARCHAR(50),surname VARCHAR(50),month_born INT,age INT);
 
 -- 2.Insert data into your table using the INSERT commands. 
 --     If you are storing a varchar it must be in " " but if you are storing a int it doesn't need any " "
+INSERT INTO people (firstname, surname, month_born, age) VALUES ("Oscar", "Guzman", 6, 18);
+INSERT INTO people (firstname, surname, month_born, age) VALUES ("John", "Doe", 1, 30);
+
 
 
 
 -- 3.Display your data and your table.
+SELECT * FROM people;
