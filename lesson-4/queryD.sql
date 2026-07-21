1639e6bdb8389b148ca89f546c4497353f24b7ab
Oscar <oguzman@basecampcodingacademy.org>
2026-07-21 18:25:41 -0500

    Lesson 4 completed

--- lesson-4/queryD.sql
+++ lesson-4/queryD.sql
@@ -1,5 +1,5 @@
 -- The data stored in the planets database are: planetName, numMoons, firstYearRecorded, distanceFromSun and discoveredBy
 
  -- Create a SQL query below so that it displays the planet name, number of moons and year discovered but order results by the number of moons in descending order.
- 
+ SELECT planetName, numMoons, firstYearRecorded FROM planets ORDER BY numMoons DESC;
  -- Remember to refer to the CheatSheet.jpg to help you.
