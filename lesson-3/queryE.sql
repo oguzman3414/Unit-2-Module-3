3cff62c7c739f37f8ff944150037b1003b910c64
Oscar <oguzman@basecampcodingacademy.org>
2026-07-21 17:58:47 -0500

    Lesson 3 completed

--- lesson-3/queryE.sql
+++ lesson-3/queryE.sql
@@ -1,5 +1,5 @@
 -- The data stored in the instagram database are: firstName, surname, career, followersMillions and instagramHandle
 
  -- Edit the SQL query below so that it displays on the first name, surname and instagram handle of those from our top 10 instagram followers database who have less than or equal to 155 million followers. In this search remember to just search for 155 you do not need to write out the number in full. 
- 
+ SELECT firstName, surname, instagramHandle FROM instagram WHERE followersMillions <= 155;
  -- Remember to refer to the CheatSheet.jpg to help you.
